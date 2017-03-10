3634a04ac41964da343a067b61e1d1b159816fc3
Orhan YILMAZ <orhan__yilmazz@hotmail.com>
2017-03-10 15:30:43 +0200

    Create Full_Time.java

---
+++ Full_Time.java
@@ -0,0 +1,12 @@
+
+public class Full_Time extends Employee {
+	private int dayOfWork;
+
+	public int getDayOfWork() {
+		return dayOfWork;
+	}
+
+	public void setDayOfWork(int dayOfWork) {
+		this.dayOfWork = dayOfWork;
+	}
+}
